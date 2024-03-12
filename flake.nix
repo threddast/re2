693b6544aa3e53f0b3e7254244427c63ae6d2561
Erik Giorgis <git@egiorg.is>
2024-03-12 15:56:26 +0100

    chore: add nix flake

---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+  description = "A flake for RE2 and related development tools";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils,  ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      with import nixpkgs { inherit system; }; {
+        devShells.default = mkShell {
+          nativeBuildInputs = [ pkg-config gnumake ];
+          buildInputs = [ abseil-cpp gtest gbenchmark ];
+        };
+      });
+}
